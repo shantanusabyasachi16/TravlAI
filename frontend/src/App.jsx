1675c9a86561a7e778d2a742b9a84accc5153e08
Shantanu Swain <137885216+shantanusabyasachi16@users.noreply.github.com>
2025-02-08 00:48:56 +0530

    installing shadcn

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import React from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-   <div>shantanu</div>
+   <div className="text-3xl font-bold underline">shantanu</div>
   )
 }
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path"
+import tailwindcss from '@tailwindcss/vite'
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
