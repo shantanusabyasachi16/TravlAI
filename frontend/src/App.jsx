4774a2fd5964f8fda1f7bd95cea970e0dd0595a1
Shantanu Swain <137885216+shantanusabyasachi16@users.noreply.github.com>
2025-02-12 16:34:46 +0530

    installed shadcn ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import React from 'react'
+import { Button } from './components/ui/button'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-   <div className="text-3xl font-bold underline">shantanu</div>
+    <>
+   
+   </>
   )
 }
 
